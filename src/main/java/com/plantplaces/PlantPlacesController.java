1389bb8a05d8a00e69da8d1dd35fc0d615865b18
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-27 11:52:59 -0400

    added some css and cleaned up code for submission

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,19 +1,36 @@
 package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 /**
  * @author Administrator
  * handle the default
- * @returns
+ * @return
  */
 
 
 @Controller
 public class PlantPlacesController {
 
-	@RequestMapping("/start")
+	@RequestMapping(value="/start", method=RequestMethod.GET)
 	public String start() {
 		
 		return "start";
 	}
+	
+	@PostMapping("/start")
+	public String create() {
+		return "start";
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		
+		return "start";
+	}
+
 }
+
